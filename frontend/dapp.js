f30d243b3d0d17a8912fe92ae192a8f9b3febf22
Sarah Rupprecht <sruppre@asu.edu>
2021-09-18 16:06:24 -0500

    custom edits

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // @TODO: Update this address to match your deployed MartianMarket contract!
-const contractAddress = "0x7a377fAd8c7dB341e662c93A79d0B0319DD3DaE8";
+const contractAddress = "0xD85936Fd01cD89B212624a0A0546ae19c902E467";
 
 const dApp = {
   ethEnabled: function() {
